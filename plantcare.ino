d676c7ca21ccecd8cebc0c60d60b1c3cca074e49
lyntoo <lyntooo@gmail.com>
2016-10-30 20:28:10 -0400

    Add files via upload
    Add comment

--- plantcare.ino
+++ plantcare.ino
@@ -1,3 +1,33 @@
+/*This project of self watering system is done for complete House Plantcare
+ * Monitor every plant soil moisture
+ * Monitor current temperature and air huidity
+ * Watering plant on threshold
+ * 
+ * to do
+ * include ph claibration probe
+ * include lcd menu
+ * include sound alarm
+ * include low level tank water sensor
+ * 
+ * 
+ * Arduino House Plant self watering system
+ *Arduino Mega256
+ *Arduino Mega256 Sensor Shield
+ *LCD2004 lcd 20x4 I2C Crystalfont 
+ *16 Relay Module low level trigger
+ *Arduino Time Clock
+ *12vdc Solenoid (from coffee machine)
+ *12vdc Car Winshield Washer pump
+ *DHT11 (temperature and humidity sensor)
+ * * 
+ */
+
+
+
+
+
+
+
 // libraries definition
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
