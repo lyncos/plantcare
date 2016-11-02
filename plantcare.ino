d2122d3b70d7d98000f8c4a7ef8b727c7a65ea3d
lyntoo <lyntooo@gmail.com>
2016-11-01 20:33:03 -0400

    Update plantcare.ino

--- plantcare.ino
+++ plantcare.ino
@@ -2,15 +2,16 @@
  * Monitor every plant soil moisture
  * Monitor current temperature and air huidity
  * Watering plant on threshold
+ * Low level water tank sound alarm
  * 
- * to do
+ * thing to do
  * include ph claibration probe
  * include lcd menu
- * include sound alarm
  * 
  * 
  * 
- * Arduino House Plant self watering system
+ * Hardware
+ *Arduino House Plant self watering system
  *Arduino Mega256
  *Arduino Mega256 Sensor Shield
  *LCD2004 lcd 20x4 I2C Crystalfont 
@@ -20,6 +21,7 @@
  *12vdc Car Winshield Washer pump
  *DHT11 (temperature and humidity sensor)
  *low level tank water sensor
+ *Piedzo buzzer
  * * 
  */
 
