cacd47b9b9c52df5420755e77667a65ff5b8efdf
lyncos@lyntoo.net <lyncos@lyntoo.com>
2016-11-03 17:58:39 -0400

    fix indentation

--- plantcare.ino
+++ plantcare.ino
@@ -42,12 +42,12 @@ SimpleDHT11 dht11;
 
 // notes in the melody:
 int melody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+	NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
 };
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {
-  4, 8, 8, 4, 4, 4, 4, 4
+	4, 8, 8, 4, 4, 4, 4, 4
 };
 
 
@@ -69,20 +69,20 @@ int plantsEnable = false;
 byte temperature = 0;
 byte humidity = 0;
 int waterlevelsensor = 0;
- 
+
 // system messages
 const char *string_table[] =
 {   
-  "  Lyncos & Manitoo",
-  " Plants readings...",
-  "      Dry soil",
-   "     Moist soil",
-  "     Soggy soil",
-  "The water pump is on",
-  "       Manisa",
-  "   Watering System",
-  "    Please wait!",
-   "         by"
+	"  Lyncos & Manitoo",
+	" Plants readings...",
+	"      Dry soil",
+	"     Moist soil",
+	"     Soggy soil",
+	"The water pump is on",
+	"       Manisa",
+	"   Watering System",
+	"    Please wait!",
+	"         by"
 };
 
 
@@ -92,257 +92,257 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 
 // SEC = 800
 void setup() {
-   // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
+	// iterate over the notes of the melody:
+	for (int thisNote = 0; thisNote < 8; thisNote++) {
 
-    // to calculate the note duration, take one second
-    // divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000 / noteDurations[thisNote];
-    tone(3, melody[thisNote], noteDuration);
+		// to calculate the note duration, take one second
+		// divided by the note type.
+		//e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+		int noteDuration = 1000 / noteDurations[thisNote];
+		tone(3, melody[thisNote], noteDuration);
 
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-    // stop the tone playing:
-    noTone(3);
-  }
+		// to distinguish the notes, set a minimum time between them.
+		// the note's duration + 30% seems to work well:
+		int pauseBetweenNotes = noteDuration * 1.30;
+		delay(pauseBetweenNotes);
+		// stop the tone playing:
+		noTone(3);
+	}
 
 
- // serial initialization
-  Serial.begin(19200);
-  
-  // LCD initialization
-  lcd.init();          
-  lcd.backlight();     // with Backlight
-  lcd.clear();         // clearscreen
+	// serial initialization
+	Serial.begin(19200);
 
-  lcd.setCursor(0,0);
-  lcd.print(string_table[6]); 
-  lcd.setCursor(0,1);
-  lcd.print(string_table[7]); 
-  lcd.setCursor(0,2);
-  lcd.print(string_table[9]); 
-  lcd.setCursor(0,3);
-  lcd.print(string_table[0]);
-  delay(3000);
-  lcd.clear();
-  // Wire initialization
-  Wire.begin();
-  
-  // RTC initialization
-  RTC.begin();
-  if (!RTC.isrunning()){
-    // date and time adjust as the PC computer date and time
-    RTC.adjust(DateTime(__DATE__, __TIME__)); 
-  }
-  
+	// LCD initialization
+	lcd.init();          
+	lcd.backlight();     // with Backlight
+	lcd.clear();         // clearscreen
 
- for ( int i = 0; i < nbPlant; i++)
-  {
-    pinMode(plantActuatorPins[i], OUTPUT);
-    digitalWrite(plantActuatorPins[i], HIGH);
-  }
-  pinMode(pumpPin, OUTPUT);
-  digitalWrite(pumpPin, HIGH);
+	lcd.setCursor(0,0);
+	lcd.print(string_table[6]); 
+	lcd.setCursor(0,1);
+	lcd.print(string_table[7]); 
+	lcd.setCursor(0,2);
+	lcd.print(string_table[9]); 
+	lcd.setCursor(0,3);
+	lcd.print(string_table[0]);
+	delay(3000);
+	lcd.clear();
+	// Wire initialization
+	Wire.begin();
+
+	// RTC initialization
+	RTC.begin();
+	if (!RTC.isrunning()){
+		// date and time adjust as the PC computer date and time
+		RTC.adjust(DateTime(__DATE__, __TIME__)); 
+	}
+
+
+	for ( int i = 0; i < nbPlant; i++)
+	{
+		pinMode(plantActuatorPins[i], OUTPUT);
+		digitalWrite(plantActuatorPins[i], HIGH);
+	}
+	pinMode(pumpPin, OUTPUT);
+	digitalWrite(pumpPin, HIGH);
 }
 
 
 unsigned int mainCounter = 0;
 void loop() {
- 
-  // RTC parameters definition
-  DateTime myRTC = RTC.now();
-  int H = myRTC.hour();
-  int M = myRTC.minute();
-  int S = myRTC.second();
-  unsigned long currentMillis = millis();
-  
- // if low water level: plays the low level alarm
- 
-  
-  pumpActive = false;
-  for (int i = 0; i < nbPlant; i++)
-  {
-    if (plantActuatorActive[i])
-    {
-      digitalWrite(plantActuatorPins[i], LOW); 
-      pumpActive = true;
-      digitalWrite(pumpPin, LOW);
-       {
-         if(waterlevelsensor > 528)
-  {
-   waterlevelsensor = analogRead(waterlevelPin);
-   pumpActive = false;
-   digitalWrite(pumpPin, LOW); 
-   tone(piezoPin, 1000, 500);
-  } 
-       }
-    }
-    else
-    {
-      digitalWrite(plantActuatorPins[i], HIGH);  
-    }
-  }
- 
-  if (pumpActive)
-  {
-   digitalWrite(pumpPin, LOW);
-  }
-  else
-  {
-   digitalWrite(pumpPin, HIGH);
-  }
- 
-  // MAIN LOOP
-  if (currentMillis - previousMillisMain >= mainInterval) 
-  {
-    previousMillisMain = currentMillis;
 
-    mainCounter++;
-    if (mainCounter == 5)
-    { 
-      lcd.clear();
-      plantsEnable = false;
-      dateEnable = true;
-    }
-    if (mainCounter == 10)
-    {
-      lcd.clear();
-      plantsEnable = true;
-      dateEnable = false;
-    }  
-    if (mainCounter > 10)
-    {
-      mainCounter = 0;
-    }
-  }
-  
-  if (plantsEnable)
-   if (dht11.read(pinDHT11, &temperature, &humidity, NULL )) 
-  {
- 
-      
-      lcd.setCursor(0,0);
-      lcd.print("P1:");
-      lcd.print(plantSensorValues[0]);
-      lcd.setCursor(14,0);
-      lcd.print("P2:");
-      lcd.print(plantSensorValues[1]);
-      lcd.setCursor(7,1);
-      lcd.print("P3:");
-      lcd.print(plantSensorValues[2]);
-      lcd.setCursor(0,2);
-      lcd.print("P4:");
-      lcd.print(plantSensorValues[3]);
-      lcd.setCursor(14,2);
-      lcd.print("P5:");
-      lcd.print(plantSensorValues[4]);
-      lcd.setCursor(0,3);  
-      lcd.print((int)temperature); lcd.print("*C");
-      lcd.setCursor(16,3); 
-      lcd.print((int)humidity); lcd.print("%H");    
-    }
+	// RTC parameters definition
+	DateTime myRTC = RTC.now();
+	int H = myRTC.hour();
+	int M = myRTC.minute();
+	int S = myRTC.second();
+	unsigned long currentMillis = millis();
 
- 
-  if (dateEnable)
-  {
-    RightHour();
-  }
+	// if low water level: plays the low level alarm
 
 
-// reading sensor every interval and print some debug
-  if (currentMillis - previousMillis >= interval) 
-  {
-    previousMillis = currentMillis;
-      
-   Serial.println("------- PLANT READINGS ----------");
-   for (int thisPlantSensor = 0; thisPlantSensor < nbPlant; thisPlantSensor++)
-   {
-        plantSensorValues[thisPlantSensor] = analogRead(plantSensorPin[thisPlantSensor]);
-        Serial.print("Plant #");
-        Serial.print(thisPlantSensor);
-        Serial.print(":");
-       
-        // Set the sensor into error
-        if (plantSensorValues[thisPlantSensor] > 1000)
-        {
-          plantErrors[thisPlantSensor] = true;
-        }
-        if (plantErrors[thisPlantSensor] == false)
-        {
-          Serial.print(plantSensorValues[thisPlantSensor]);      
- 
-          if (plantSensorValues[thisPlantSensor] > plantSensorSetPoints[thisPlantSensor])
-          {
-            Serial.println("     GIVE ME WATER!!!!!");
-            plantActuatorActive[thisPlantSensor] = true;
-          }
-           else
-          {
-            Serial.println("no water needed");
-            plantActuatorActive[thisPlantSensor] = false;
-          }  
- 
-        }
-        else
-        {
-          Serial.println("Sensor Error");
-        }
-   }
-   Serial.print("temperature ");Serial.print((int)temperature);Serial.print("*C");
-   Serial.println("");
-   Serial.print("humidity ");Serial.print((int)humidity);Serial.print("%H"); 
-   Serial.println("");
-   waterlevelsensor = analogRead(waterlevelPin);
-   Serial.print("water level ");Serial.print(waterlevelsensor);
-   Serial.println("");
-   Serial.println("");
-   Serial.println("");
- }
+	pumpActive = false;
+	for (int i = 0; i < nbPlant; i++)
+	{
+		if (plantActuatorActive[i])
+		{
+			digitalWrite(plantActuatorPins[i], LOW); 
+			pumpActive = true;
+			digitalWrite(pumpPin, LOW);
+			{
+				if(waterlevelsensor > 528)
+				{
+					waterlevelsensor = analogRead(waterlevelPin);
+					pumpActive = false;
+					digitalWrite(pumpPin, LOW); 
+					tone(piezoPin, 1000, 500);
+				} 
+			}
+		}
+		else
+		{
+			digitalWrite(plantActuatorPins[i], HIGH);  
+		}
+	}
+
+	if (pumpActive)
+	{
+		digitalWrite(pumpPin, LOW);
+	}
+	else
+	{
+		digitalWrite(pumpPin, HIGH);
+	}
+
+	// MAIN LOOP
+	if (currentMillis - previousMillisMain >= mainInterval) 
+	{
+		previousMillisMain = currentMillis;
+
+		mainCounter++;
+		if (mainCounter == 5)
+		{ 
+			lcd.clear();
+			plantsEnable = false;
+			dateEnable = true;
+		}
+		if (mainCounter == 10)
+		{
+			lcd.clear();
+			plantsEnable = true;
+			dateEnable = false;
+		}  
+		if (mainCounter > 10)
+		{
+			mainCounter = 0;
+		}
+	}
+
+	if (plantsEnable)
+		if (dht11.read(pinDHT11, &temperature, &humidity, NULL )) 
+		{
+
+
+			lcd.setCursor(0,0);
+			lcd.print("P1:");
+			lcd.print(plantSensorValues[0]);
+			lcd.setCursor(14,0);
+			lcd.print("P2:");
+			lcd.print(plantSensorValues[1]);
+			lcd.setCursor(7,1);
+			lcd.print("P3:");
+			lcd.print(plantSensorValues[2]);
+			lcd.setCursor(0,2);
+			lcd.print("P4:");
+			lcd.print(plantSensorValues[3]);
+			lcd.setCursor(14,2);
+			lcd.print("P5:");
+			lcd.print(plantSensorValues[4]);
+			lcd.setCursor(0,3);  
+			lcd.print((int)temperature); lcd.print("*C");
+			lcd.setCursor(16,3); 
+			lcd.print((int)humidity); lcd.print("%H");    
+		}
+
+
+	if (dateEnable)
+	{
+		RightHour();
+	}
+
+
+	// reading sensor every interval and print some debug
+	if (currentMillis - previousMillis >= interval) 
+	{
+		previousMillis = currentMillis;
+
+		Serial.println("------- PLANT READINGS ----------");
+		for (int thisPlantSensor = 0; thisPlantSensor < nbPlant; thisPlantSensor++)
+		{
+			plantSensorValues[thisPlantSensor] = analogRead(plantSensorPin[thisPlantSensor]);
+			Serial.print("Plant #");
+			Serial.print(thisPlantSensor);
+			Serial.print(":");
+
+			// Set the sensor into error
+			if (plantSensorValues[thisPlantSensor] > 1000)
+			{
+				plantErrors[thisPlantSensor] = true;
+			}
+			if (plantErrors[thisPlantSensor] == false)
+			{
+				Serial.print(plantSensorValues[thisPlantSensor]);      
+
+				if (plantSensorValues[thisPlantSensor] > plantSensorSetPoints[thisPlantSensor])
+				{
+					Serial.println("     GIVE ME WATER!!!!!");
+					plantActuatorActive[thisPlantSensor] = true;
+				}
+				else
+				{
+					Serial.println("no water needed");
+					plantActuatorActive[thisPlantSensor] = false;
+				}  
+
+			}
+			else
+			{
+				Serial.println("Sensor Error");
+			}
+		}
+		Serial.print("temperature ");Serial.print((int)temperature);Serial.print("*C");
+		Serial.println("");
+		Serial.print("humidity ");Serial.print((int)humidity);Serial.print("%H"); 
+		Serial.println("");
+		waterlevelsensor = analogRead(waterlevelPin);
+		Serial.print("water level ");Serial.print(waterlevelsensor);
+		Serial.println("");
+		Serial.println("");
+		Serial.println("");
+	}
 }
 
 // Real Time Clock Function
 void RightHour()
 {
-  DateTime Now = RTC.now();
-  String clock_date = "  Date: ";
-  String clock_hour = "   Time: ";
-  
-  int _day = Now.day();
-  int _month = Now.month();
-  int _year = Now.year();
-  
-  clock_date += fixZero(_day);
-  clock_date += "/";
-  clock_date += fixZero(_month);
-  clock_date += "/";
-  clock_date += _year;
-  
-  int _hour = Now.hour();
-  int _minute = Now.minute();
-  int _second = Now.second();
-  
-  clock_hour += fixZero(_hour);
-  clock_hour += ":";
-  clock_hour += fixZero(_minute);
-  clock_hour += ":";
-  clock_hour += fixZero(_second);
+	DateTime Now = RTC.now();
+	String clock_date = "  Date: ";
+	String clock_hour = "   Time: ";
 
-  lcd.setCursor(0, 0);
-  lcd.print(clock_date);
-  lcd.setCursor(0, 1);
-  lcd.print(clock_hour);
-  lcd.setCursor(0,3);
-  lcd.print("Reading sensors...");  
-  delay(50);
+	int _day = Now.day();
+	int _month = Now.month();
+	int _year = Now.year();
+
+	clock_date += fixZero(_day);
+	clock_date += "/";
+	clock_date += fixZero(_month);
+	clock_date += "/";
+	clock_date += _year;
+
+	int _hour = Now.hour();
+	int _minute = Now.minute();
+	int _second = Now.second();
+
+	clock_hour += fixZero(_hour);
+	clock_hour += ":";
+	clock_hour += fixZero(_minute);
+	clock_hour += ":";
+	clock_hour += fixZero(_second);
+
+	lcd.setCursor(0, 0);
+	lcd.print(clock_date);
+	lcd.setCursor(0, 1);
+	lcd.print(clock_hour);
+	lcd.setCursor(0,3);
+	lcd.print("Reading sensors...");  
+	delay(50);
 }
 
 String fixZero(int i)
 {
-  String ret;
-  if (i < 10) ret += "0";
-  ret += i;
-  return ret;
+	String ret;
+	if (i < 10) ret += "0";
+	ret += i;
+	return ret;
 }
